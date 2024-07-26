8cbdac13a03550b2f27c8b479a8308d0e4126f7c
Christian Cameron Rapp <chrap3964@uib.no>
2024-07-26 15:42:35 +0200

    trying to fix duplicate notification problems

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -17,7 +17,7 @@ const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function(payload) {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  const notificationTitle = payload.notification.title;
+  const notificationTitle = payload.data.title;
   const notificationOptions = {
     body: payload.notification.body,
     icon: '/media/idfMm1ADA9_1717842206506.svg',
