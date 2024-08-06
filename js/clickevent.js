7bcf0a604bb0849e73514bcb32cd638767c9f2a5
Christian Cameron Rapp <chrap3964@uib.no>
2024-08-06 20:53:53 +0200

    changes in how the navigation bar navigates

---
+++ js/clickevent.js
@@ -0,0 +1,11 @@
+document.getElementById('nav-innleggstavle').addEventListener('click', () => {
+    window.location.href = '/index.html';
+});
+
+document.getElementById('nav-arbeidsportal').addEventListener('click', () => {
+    window.location.href = '/html/arbeidsportal.html';
+});
+
+document.getElementById('nav-vikarportal').addEventListener('click', () => {
+    window.location.href = '/html/vikarportal.html';
+});
